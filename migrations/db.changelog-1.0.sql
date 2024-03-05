847f59395ffe8ef93476041a4cfa5b3122b845c5
VictorGalkevich <2vektor54@gmail.com>
2024-03-05 21:06:28 +0300

    did first 3 tasks

---
+++ migrations/db.changelog-1.0.sql
@@ -0,0 +1,28 @@
+--liquibase formatted sql
+
+--changeset VictorGalkevich:1
+CREATE TABLE IF NOT EXISTS chats
+(
+    id            BIGSERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_by VARCHAR(64)
+);
+
+--changeset VictorGalkevich:2
+CREATE TABLE IF NOT EXISTS links
+(
+    id              BIGSERIAL PRIMARY KEY,
+    url             VARCHAR(256) UNIQUE NOT NULL,
+    host             VARCHAR(16) NOT NULL,
+    protocol             VARCHAR(16) NOT NULL,
+    last_updated_at TIMESTAMP WITH TIME ZONE
+);
+
+--changeset VictorGalkevich:3
+CREATE TABLE IF NOT EXISTS assignment
+(
+    id BIGSERIAL PRIMARY KEY,
+    chat_id BIGINT REFERENCES chats (id),
+    link_id BIGINT REFERENCES links (id),
+    UNIQUE (chat_id, link_id)
+)
